3508f083fc9f80f80508480b73a54edc8db62b24
Helen Choi <helenchungg@gmail.com>
2020-03-25 13:39:45 -0700

    Showing fortune content working correctly

--- components/app.js
+++ components/app.js
@@ -1,6 +1,8 @@
 class App {
-  constructor(input) {
+  constructor(input,contentElement) {
     this.input = input;
+    this.zodiac = null;
+    this.contentElement = contentElement;
     this.handleFortuneSuccess = this.handleFortuneSuccess.bind(this);
     this.getHeroSuccess = this.getHeroSuccess.bind(this);
     this.getFortune = this.getFortune.bind(this);
@@ -15,6 +17,7 @@ class App {
       url: 'https://aztro.sameerkumar.website?sign=' + zodiac +'&day=today',
       success: this.handleFortuneSuccess
     });
+    this.zodiac = zodiac;
   }
   handleFortuneSuccess(data) {
     this.showFortune(data);
@@ -30,6 +33,35 @@ class App {
     console.log(data)
   }
   showFortune(data) {
-    console.log(data.description);
+    console.log(data);
+    var header = document.createElement('header');
+    var greeting = document.createElement('h2');
+    var fortuneElement = document.createElement('p');
+    var divLeft = document.createElement('div');
+    var divRight = document.createElement('div');
+    var luckyNumber = document.createElement('p');
+    var luckyTime = document.createElement('p');
+    var color = document.createElement('p');
+    var compatibility = document.createElement('p');
+
+    greeting.textContent = 'Hello ' + this.zodiac + '!';
+    fortuneElement.textContent = data.description;
+    divLeft.classList.add('fortune-details', 'right');
+    divRight.classList.add('fortune-details', 'right');
+    luckyNumber.textContent = 'Lucky Number: ' + data.lucky_number;
+    luckyTime.textContent = 'Lucky Time: ' + data.lucky_time;
+    color.textContent = 'Color: ' + data.color;
+    compatibility.textContent = 'Compatibility: ' + data.compatibility;
+
+    header.appendChild(greeting);
+    header.appendChild(fortuneElement);
+    divLeft.appendChild(luckyNumber);
+    divLeft.appendChild(luckyTime);
+    divLeft.appendChild(color);
+    divLeft.appendChild(compatibility);
+
+    this.contentElement.appendChild(header);
+    this.contentElement.appendChild(divLeft);
+    this.contentElement.appendChild(divRight);
   }
 }
--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var form = document.querySelector("form");
 var bdayElement = form.querySelector("input");
-// var zodiac = 'virgo';
 var contentElement = document.getElementById("fortune");
 
 var input = new Input(bdayElement, form);
