19b42679609dceed1adedf40aa3df4da3babe050
Helen Choi <helenchungg@gmail.com>
2020-03-29 01:19:09 -0700

    created findZodiac method

--- components/input.js
+++ components/input.js
@@ -8,6 +8,36 @@ class Input {
     this.handleSubmit = this.handleSubmit.bind(this)
     this.form.addEventListener('submit', this.handleSubmit);
   }
+  findZodiac () {
+    this.zodiac = '';
+    var date = this.bdayElement.value.split('-');
+    var dayNumber = parseInt(date[2]);
+    if (date[1] === '03' && dayNumber >= 21 || date[1] === '04' && dayNumber <= 19) {
+      this.zodiac = 'aries';
+    } else if (date[1] === '04' && dayNumber >= 20 || date[1] === '05' && dayNumber <= 20) {
+      this.zodiac = 'taurus';
+    } else if (date[1] === '05' && dayNumber >= 21 || date[1] === '06' && dayNumber <= 20) {
+      this.zodiac = 'gemini';
+    } else if (date[1] === '06' && dayNumber >= 21 || date[1] === '07' && dayNumber <= 22) {
+      this.zodiac = 'cancer';
+    } else if (date[1] === '07' && dayNumber >= 23 || date[1] === '08' && dayNumber <= 22) {
+      this.zodiac = 'leo';
+    } else if (date[1] === '08' && dayNumber >= 23 || date[1] === '09' && dayNumber <= 22) {
+      this.zodiac = 'virgo';
+    } else if (date[1] === '09' && dayNumber >= 23 || date[1] === '10' && dayNumber <= 22) {
+      this.zodiac = 'libra';
+    } else if (date[1] === '10' && dayNumber >= 23 || date[1] === '11' && dayNumber <= 21) {
+      this.zodiac = 'scorpio';
+    } else if (date[1] === '11' && dayNumber >= 22 || date[1] === '12' && dayNumber <= 21) {
+      this.zodiac = 'sagittarius';
+    } else if (date[1] === '12' && dayNumber >= 22 || date[1] === '01' && dayNumber <= 19) {
+      this.zodiac = 'capricorn';
+    } else if (date[1] === '01' && dayNumber >= 20 || date[1] === '02' && dayNumber <= 18) {
+      this.zodiac = 'aquarius';
+    } else if (date[1] === '02' && dayNumber >= 19 || date[1] === '03' && dayNumber <= 20) {
+      this.zodiac = 'pisces';
+    }
+  }
   onSubmit(getFortune, getHero, smoothScroll) {
     this.getFortune = getFortune;
     this.getHero = getHero;
@@ -15,34 +45,7 @@ class Input {
   }
   handleSubmit(event) {
     event.preventDefault();
-    this.zodiac = '';
-    var date = this.bdayElement.value.split('-');
-    var dayNumber = parseInt(date[2]);
-      if (date[1] === '03' && dayNumber >= 21 || date[1] === '04' && dayNumber <= 19) {
-        this.zodiac = 'aries';
-      } else if (date[1] === '04' && dayNumber >= 20 || date[1] === '05' && dayNumber <= 20) {
-        this.zodiac = 'taurus';
-      } else if (date[1] === '05' && dayNumber >= 21 || date[1] === '06' && dayNumber <= 20) {
-        this.zodiac = 'gemini';
-      } else if (date[1] === '06' && dayNumber >= 21 || date[1] === '07' && dayNumber <= 22) {
-        this.zodiac = 'cancer';
-      } else if (date[1] === '07' && dayNumber >= 23 || date[1] === '08' && dayNumber <= 22) {
-        this.zodiac = 'leo';
-      } else if (date[1] === '08' && dayNumber >= 23 || date[1] === '09' && dayNumber <= 22) {
-        this.zodiac = 'virgo';
-      } else if (date[1] === '09' && dayNumber >= 23 || date[1] === '10' && dayNumber <= 22) {
-        this.zodiac = 'libra';
-      } else if (date[1] === '10' && dayNumber >= 23 || date[1] === '11' && dayNumber <= 21) {
-        this.zodiac = 'scorpio';
-      }else if (date[1] === '11' && dayNumber >= 22 || date[1] === '12' && dayNumber <= 21) {
-        this.zodiac = 'sagittarius';
-      } else if (date[1] === '12' && dayNumber >= 22 || date[1] === '01' && dayNumber <= 19) {
-        this.zodiac = 'capricorn';
-      } else if (date[1] === '01' && dayNumber >= 20 || date[1] === '02' && dayNumber <= 18) {
-        this.zodiac = 'aquarius';
-      } else if (date[1] === '02' && dayNumber >= 19 || date[1] === '03' && dayNumber <= 20) {
-        this.zodiac = 'pisces';
-      }
+    this.findZodiac();
     this.getFortune(this.zodiac);
     this.getHero();
     event.target.reset();
